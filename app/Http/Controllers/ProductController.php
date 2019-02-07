9cf0e80c00ba50be4f9a77cecbd2c51bed84fc5d
sukri <sukri@fivejack.com>
2019-02-07 10:26:55 +0700

    up

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    protected $product_service;
+
+    public function __construct(ProductService $ProductService)
+    {
+        $this->product_service = $ProductService;
+    }
+
+    public function view(Request $request)
+    {
+        $id = '';
+        if ($request->has('id')) {
+            $id = $request->id;
+        }
+
+        return $this->product_service->view($id);
+    }
+
+    public function search()
+    {
+        return $this->product_service->search();
+    }
+
+    public function detail(Request $request)
+    {
+        return $this->product_service->view($request->id);
+    }
+
+    public function delete(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'id' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $array = [
+                'success' => false,
+                'message' => $validator->errors()->first(),
+            ];
+
+            return $array;
+        }
+
+        return $this->product_service->delete($request->id);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $array = [
+                'success' => false,
+                'message' => $validator->errors()->first(),
+            ];
+
+            return $array;
+        }
+
+        return $this->product_service->update($request->id, $request->name, $request->link);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $array = [
+                'success' => false,
+                'message' => $validator->errors()->first(),
+            ];
+
+            return $array;
+        }
+
+        return $this->product_service->store($request->name, $request->link);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+$router->post('/login', 'UserController@login');
+$router->post('/logout', 'UserController@logout');
+$router->get('/user/search', 'UserController@search');
+
+//PRODUCT
+$router->post('/product', ['middleware' => 'auth', 'uses' => 'ProductController@view']);
+$router->post('/product/store', ['middleware' => 'auth', 'uses' => 'ProductController@store']);
+$router->post('/product/update', ['middleware' => 'auth', 'uses' => 'ProductController@update']);
+$router->post('/product/delete', ['middleware' => 'auth', 'uses' => 'ProductController@delete']);
+$router->get('/product/search', 'ProductController@search');
+
+//ORDER
+$router->post('/order', ['middleware' => 'auth', 'uses' => 'OrderController@view']);
+$router->post('/order/store', ['middleware' => 'auth', 'uses' => 'OrderController@store']);
+$router->post('/order/update', ['middleware' => 'auth', 'uses' => 'OrderController@update']);
+$router->post('/order/delete', ['middleware' => 'auth', 'uses' => 'OrderController@delete']);
+
+$router->post('/order/reporter', ['middleware' => 'auth', 'uses' => 'OrderController@reporter']);
+$router->post('/order/update_finance', ['middleware' => 'auth', 'uses' => 'OrderController@update_finance']);
